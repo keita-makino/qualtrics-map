e745ab0ba23cd1a81e95858782a18b69ee5a0c68
Keita Makino <kemakino@ucdavis.edu>
2024-03-04 17:46:32 -0800

    mapbox beta 5

--- src/components/ClearButton.tsx
+++ src/components/ClearButton.tsx
@@ -1,13 +1,18 @@
 import { Grid, Button, Typography, makeStyles } from '@mui/material';
 import React, { useContext } from 'react';
 import { useUpdate } from '../store';
+import styled from '@emotion/styled';
 
 export type ClearButtonProps = {};
 
 export const ClearButton: React.FC<ClearButtonProps> = (
-  props: ClearButtonProps
+  props: ClearButtonProps,
 ) => {
   const update = useUpdate();
+
+  const StyledButton = styled(Button)`
+    color: white !important;
+  `;
 
   return (
     <Grid container justifyContent={'center'}>
@@ -20,15 +25,15 @@ export const ClearButton: React.FC<ClearButtonProps> = (
         sm={6}
         xs={6}
         justifyContent={'center'}
-        style={{ position: 'relative', top: '-3rem' }}
+        style={{ position: 'relative', top: '-4rem' }}
       >
-        <Button
+        <StyledButton
           variant={'contained'}
           color={'primary'}
           onClick={() => update({ type: 'CLEAR_INPUT' })}
         >
           <Typography variant={'button'}>Clear Pin(s)</Typography>
-        </Button>
+        </StyledButton>
       </Grid>
     </Grid>
   );
